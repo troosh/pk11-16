189d898fb41cb6df1c3c7a1a2443b9d014b404ee
Alexander Troosh <trush@yandex.ru>
2018-08-08 13:45:26 +0300

    Script for spit schematic

---
+++ Split_Sch/split_sch.sh
@@ -0,0 +1,2 @@
+pdfposter -p 2x2a4 pk11-16-sch-20180617.pdf --first 1 --last 1 pk11-16-sch-20180617-2x2-a4.page1.pdf
+pdfposter -p 2x1a4 pk11-16-sch-20180617.pdf --first 2 --last 3 pk11-16-sch-20180617-2x1-a4.page2-3.pdf
