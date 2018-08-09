97511fab7bb647ace46db491eb2f683c6b23aecc
Alexander Troosh <trush@yandex.ru>
2018-08-09 12:45:47 +0300

    Script for JED -> EQN convertion

---
+++ PK11-chips/convert_jed2eqn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# trush@yandex.ru
+
+# Patched and compiled tool from MAME project
+JEDUTIL=$PWD/jedutil
+
+# 104 variants
+for device in PLS100 PAL16L8 PAL16R4 PAL16R6
+do
+  for f in $(find ./$device -iname *.jed -type f)
+  do
+    DIR=$(dirname $f)
+    BASE=$(basename $f .jed)
+    pushd $DIR >/dev/null
+    echo $BASE
+    $JEDUTIL -view $BASE.jed $device &>$BASE.eqn
+    ln -s $BASE.jed $(md5sum $BASE.jed| cut -c1-8).jed
+    ln -s $BASE.eqn $(md5sum $BASE.jed| cut -c1-8).eqn
+#    $JEDUTIL -convert $BASE.jed $BASE.bin &>$BASE.bin.txt
+    popd >/dev/null
+  done
+done
